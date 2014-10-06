01ab82f2353d94f065a42be7207930a047a6cae4
Rom-B <roman@workita.com>
2014-10-07 00:08:28 +0200

    initial

---
+++ template.php
@@ -0,0 +1,13 @@
+<?php
+/** function phptemplate_maintenance_page($content, $messages = TRUE, $partial = FALSE) {
+//  drupal_goto('/index.htm');
+//}
+function phptemplate_maintenance_page($content, $messages = FALSE, $partial = FALSE) {
+  require_once '/index.htm';
+}
+
+
+/ **
+ * @file
+ * template.php
+ */
